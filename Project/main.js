2fcc1962717886bd8296bc7525b754d8441400f6
mgschomp37 <mgschomp37@gmail.com>
2022-04-04 01:37:58 -0400

    Updated buttons, started User class

---
+++ Project/main.js
@@ -0,0 +1,8 @@
+class User{
+    constructor(id, username, pword, email, fname, lname, city, state, zip){
+        this.userID = id;
+        this.username = username;
+        this.pword = pword;
+        this
+    }
+}
